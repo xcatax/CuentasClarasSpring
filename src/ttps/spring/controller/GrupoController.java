0e27f2b65d0f962543a32d0d538913f2c46ed7b7
cata <rattocatalina@gmail.com>
2023-12-20 16:28:35 -0300

    cambios en como devovlemos las cosas del bacl

--- src/ttps/spring/controller/GrupoController.java
+++ src/ttps/spring/controller/GrupoController.java
@@ -1,9 +1,6 @@
 package ttps.spring.controller;
 
 import ttps.spring.model.*;
-import ttps.spring.model.Gasto;
-
-import ttps.spring.model.CategoriaGrupo;
 import ttps.spring.repository.CategoriaGrupoRepository;
 import ttps.spring.repository.GastoRepository;
 import ttps.spring.repository.GrupoRepository;
@@ -18,14 +15,10 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-
 import org.springframework.http.MediaType; // Asegúrate de importar MediaType
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200") 
 @RestController
 @RequestMapping(value = "/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GrupoController {
@@ -99,29 +92,33 @@ public class GrupoController {
 	    return new ResponseEntity<>(message, HttpStatus.OK);
 	}
 	
+	@GetMapping("/listarCategorias")
+	public List<CategoriaGrupo> categoriaGrupo() {
+		System.out.println("listar");
+		return categoriaRepository.findAll();
+	}
 	
 	@PostMapping()
 	@Transactional
-	public ResponseEntity<String> crearGrupo(@RequestBody Grupo grupo) {
+	public ResponseEntity<Grupo> crearGrupo(@RequestBody Grupo grupo) {
 		try {
 
 			// Verificar si ya existe un grupo con el mismo nombre
 			if (grupoRepository.findByNombre(grupo.getNombre()) != null) {
 				String message = "Existe grupo con ese nombre";
-				return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 			}
 			// Buscar la categoría
 			CategoriaGrupo cat = categoriaRepository.findByNombre(grupo.getCategoria().getNombre());
 			if (cat == null) {
 				String message = "No existe categoria con ese nombre";
-				return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 			}
 			grupo.setCategoria(cat);
-			grupoRepository.save(grupo);
-			String message = "Grupo guardado correctamente";
-			return new ResponseEntity<>(message, HttpStatus.CREATED);
+			Grupo grupoOk =	grupoRepository.save(grupo);
+			return new ResponseEntity<Grupo>(grupoOk,HttpStatus.CREATED);
 		} catch (Exception e) {
-			return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<Grupo>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
@@ -130,28 +127,27 @@ public class GrupoController {
 		System.out.println("listar");
 		return grupoRepository.findAll();
 		// return null;
-	}
-	
+	}	
 	
 	
 
 	@PutMapping("/{id}")
 	@Transactional
-	public ResponseEntity<String> actualizarGrupo(@PathVariable Long id, @RequestBody Grupo nuevoGrupo) {
+	public ResponseEntity<Grupo> actualizarGrupo(@PathVariable Long id, @RequestBody Grupo nuevoGrupo) {
 		Grupo grupoExistente = grupoRepository.findById(id); // busco el grupo a modificar
 		String messageOk = "Se modifico: ";
-		if (grupoExistente != null) { // Si lo encontre:
+		if (grupoExistente != null) {  // Si lo encontre:
 			// --------Modifica Nombre
-			if (nuevoGrupo.getNombre() != null) { // ingreso nombre?
+			/*if (nuevoGrupo.getNombre() != null) { // ingreso nombre?
 				System.out.println("Intenta modificar el nombre");
 				if (grupoRepository.findByNombre(nuevoGrupo.getNombre()) != null) { // --- El nombre ya existe?
 					String message = "Existe grupo con ese nombre";
 					return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST); // corta
 				}
 				messageOk = messageOk + "nombre, ";
-				System.out.println("El nombre esta ok se modifica");
+				System.out.println("El nombre esta ok se modifica");*/
 				grupoExistente.setNombre(nuevoGrupo.getNombre());
-			}
+			
 
 			// --------Modifica imagen
 			if (nuevoGrupo.getImagen() != 0) {
@@ -169,7 +165,7 @@ public class GrupoController {
 				CategoriaGrupo cat = categoriaRepository.findByNombre(nuevoGrupo.getCategoria().getNombre());
 				if (cat == null) {
 					String message = "No existe categoria con ese nombre";
-					return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+					return new ResponseEntity<Grupo>(HttpStatus.BAD_REQUEST);
 				} else {
 					messageOk = messageOk + " cateoria";
 					grupoExistente.setCategoria(cat);
@@ -179,13 +175,14 @@ public class GrupoController {
 				System.out.println("categoria vino null");
 			}
 
-			return new ResponseEntity<>(messageOk, HttpStatus.OK);
+			return new ResponseEntity<Grupo>(HttpStatus.OK);
 
-		} else {
-			String message = "No existe grupo con ese ID";
-			return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+		}else { 
+			//String message = "No existe grupo con ese ID";
+			return new ResponseEntity<Grupo>(HttpStatus.BAD_REQUEST);
 		}
 	}
+
 	
 	@Transactional
 	@PutMapping("/{idGrupo}/usuario")
